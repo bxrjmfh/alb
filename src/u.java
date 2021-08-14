5f5033f133f0f8bb7e144f92453c64b15d583157
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-08-14 23:55:55 +0800

    4.在不同的类中使用了另外的类的对象

--- src/u.java
+++ src/u.java
@@ -3,17 +3,34 @@ public class u
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World!");
+		dog frt =new dog(23,"ui",78);
+		dog[] dogs;
+		dogs = new dog[3];
+		dogs[0]= new dog(28,"io",4);
+		dogs[1]= new dog(28,"io",13);
+		dogs[2]= new dog(28,"io",6);
+		
+		
+		frt.bark();
+		dogs[0].bark();
+		dogs[1].bark();
+		dogs[2].bark();
 	}
-	//
+	
 }
 
 class dog
 {
-	dog oi=new dog();
-	
 	private int length;
 	private int weight;
 	private String name;
+	
+	public dog(int length ,String name ,int weight)
+	{
+		setLength(length);
+		setWeight(weight);
+		setName(name);
+	}
 	
 	public void setLength(int length)
 	{
@@ -50,12 +67,10 @@ class dog
 		if (getWeight() >= 5 && getWeight() <= 20)
 		{
 			System.out.println("yip!");
-		}
-		else if (getWeight() < 5)
+		} else if (getWeight() < 5)
 		{
 			System.out.println("ying!");
-		}
-		else if (getWeight() > 20)
+		} else if (getWeight() > 20)
 		{
 			System.out.println("Woof!");
 		}
