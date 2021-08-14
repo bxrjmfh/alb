eb85eebd3ff7240b2d86e5bca3fba858ee198f16
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-08-14 23:04:34 +0800

    20210814

--- src/u.java
+++ src/u.java
@@ -2,7 +2,60 @@ public class u
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Woeld!");
+		System.out.println("Hello World!");
 	}
+	//dog[] dogs=dog[8];
 }
 
+class dog
+{
+	private int length;
+	private int weight;
+	private String name;
+	
+	public void setLength(int length)
+	{
+		this.length = length;
+	}
+	
+	public int getLength()
+	{
+		return length;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setWeight(int weight)
+	{
+		this.weight = weight;
+	}
+	
+	public int getWeight()
+	{
+		return weight;
+	}
+	
+	void bark()
+	{
+		if (getWeight() >= 5 && getWeight() <= 20)
+		{
+			System.out.println("yip!");
+		}
+		else if (getWeight() < 5)
+		{
+			System.out.println("ying!");
+		}
+		else if (getWeight() > 20)
+		{
+			System.out.println("Woof!");
+		}
+	}
+}
